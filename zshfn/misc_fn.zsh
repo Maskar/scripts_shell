d5a9a26f466bfa4e6eab774cee60cc1d1a22c7e7
maskar <{ID}+{username}@users.noreply.github.com>
2023-12-24 16:29:00 -0600

    update

--- zshfn/misc_fn.zsh
+++ zshfn/misc_fn.zsh
@@ -6,11 +6,11 @@ function jpl() {screen -dmS JLab conda run -n depaul jupyter-lab --autoreload --
 
 function jp() {conda run -n depaul jupyter $@}
 
-function get_ip_orange() {curl -s -u dc075e55-726d-43e9-f251-faadb9c3a27d:x -X GET https://www.getflix.com.au/api/v1/addresses.json | jq --raw-output '.[].ip_address'}
+##function get_ip_orange() {curl -s -u dc075e55-726d-43e9-f251-faadb9c3a27d:x -X GET https://www.getflix.com.au/api/v1/addresses.json | jq --raw-output '.[].ip_address'}
 #function tedata_ip() {echo 41.39.89.232}
 #function tedata_ip() {echo 197.51.236.127}
-function get_ip_tedata() {curl -s -u 787ea028-6bb5-4ade-d2db-53585e02d6ce:x -X GET https://www.getflix.com.au/api/v1/addresses.json | jq --raw-output '.[].ip_address'}
+##function get_ip_tedata() {curl -s -u 787ea028-6bb5-4ade-d2db-53585e02d6ce:x -X GET https://www.getflix.com.au/api/v1/addresses.json | jq --raw-output '.[].ip_address'}
 #function get_ip_tedata() {echo 197.50.65.2}
 
-function get_ip_public() {curl checkip.amazonaws.com}
-function get_ip_local() {echo "WIFI: $(ipconfig getifaddr en0 || echo N/A)"; echo "ETH:  $(ipconfig getifaddr en7 || echo N/A)"}
+##function get_ip_public() {curl checkip.amazonaws.com}
+##function get_ip_local() {echo "WIFI: $(ipconfig getifaddr en0 || echo N/A)"; echo "ETH:  $(ipconfig getifaddr en7 || echo N/A)"}
