19a3b0ece1e29ce3eee74e918285a379eca63158
maskar <1695584+Maskar@users.noreply.github.com>
2025-08-07 17:45:26 -0500

    refactor: streamline post_zshrc.zsh by consolidating environment setup and correcting path exports

--- post_zshrc.zsh
+++ post_zshrc.zsh
@@ -1,9 +1,37 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+if command -v pyenv &>/dev/null; then
+    eval "$(pyenv init --path)"
+fi
+if command -v direnv &>/dev/null; then
+    eval "$(direnv hook zsh)"
+fi
+if command -v gh &>/dev/null; then
+    eval "$(gh copilot alias -- zsh)"
+fi
+if command -v uv &>/dev/null; then
+    eval "$(uv generate-shell-completion zsh)"
+fi
+if command -v uvx &>/dev/null; then
+    eval "$(uvx --generate-shell-completion zsh)"
+fi
+if command -v ngrok &>/dev/null; then
+    eval "$(ngrok completion)"
+fi
 
 # PATH
 export PATH="$PYENV_ROOT/bin:$PATH"
+export PATH="$PATH:$HOME/.cache/lm-studio/bin"
 export PATH="$HOMEBREW_PREFIX/opt/mysql-client/bin:$PATH"
-export PATH="$PATH:/Users/maskar/.cache/lm-studio/bin"
-export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/openjdk/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+
+# FPATH
+FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+
+# Set up fzf key bindings and fuzzy completion
+source <(fzf --zsh)
+
 
 # Aliases
 alias mm='micromamba'
@@ -13,21 +41,7 @@ alias ll='eza -l --icons'
 alias l='eza -la --icons'
 alias lt='eza --tree --level=2 --icons'
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
-eval "$(pyenv init --path)"
-eval "$(direnv hook zsh)"
-eval "$(gh copilot alias -- zsh)"
-eval "$(uv generate-shell-completion zsh)"
-eval "$(uvx --generate-shell-completion zsh)"
-
-
-# FPATH
-FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-
-# Set up fzf key bindings and fuzzy completion
-source <(fzf --zsh)
-
 # Load custom functions and environment variables
-source "$HOME/Repos/github/scripts_shell/env_vars.zsh"
-source "$HOME/Repos/github/scripts_shell/zshfn/misc_fn.zsh"
-source "$HOME/Repos/github/scripts_shell/zshfn/vpn_fn.zsh"
+source "$HOME/.scripts_shell/env_vars.zsh"
+source "$HOME/.scripts_shell/zshfn/misc_fn.zsh"
+source "$HOME/.scripts_shell/zshfn/vpn_fn.zsh"
